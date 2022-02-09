45c38852d483dd2207d00a4051a01d004924f03e
Ubuntu <ubuntu@ip-172-31-81-172.ec2.internal>
2022-02-09 08:37:13 +0000

    version 4 setup

--- demo.py
+++ demo.py
@@ -14,6 +14,8 @@ import sys
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
+import imutils
+import itertools
 
 sys.path.insert(0, 'third_party/CenterNet2/projects/CenterNet2/')
 from centernet.config import add_centernet_config
@@ -125,6 +127,80 @@ def sad_calculation(mask, lookup):
     return sad_diff, mse_diff
     # pass
 
+# to check if r2 is inside r1
+def contains(r1, r2):
+    return r1[0] < r2[0] < r2[2] < r1[2] and r1[1] < r2[1] < r2[3] < r1[3]
+
+
+def remove_contained_cnts(cnts):
+    cnt_indexes = list(itertools.combinations(range(len(cnts)), 2))
+
+    remove_indexes = []
+
+    keep_indexes = []
+
+    cnts_ = []
+
+    for index_pair in cnt_indexes:
+        indexes = [index_pair[0], index_pair[1]]
+        cnt1, cnt2 = cnts[index_pair[0]], cnts[index_pair[1]]
+        rect_0 = cv2.boundingRect(cnt1)
+        rect_0 = (rect_0[0], rect_0[1], rect_0[0] + rect_0[2], rect_0[1] + rect_0[3])
+        rect_1 = cv2.boundingRect(cnt2)
+        rect_1 = (rect_1[0], rect_1[1], rect_1[0] + rect_1[2], rect_1[1] + rect_1[3])
+
+        # rect1 inside rect0
+        if contains(rect_0, rect_1):
+            #             print(rect_0, rect_1)
+            remove_indexes.append(index_pair[1])
+
+        # rect0 inside rect1
+        elif contains(rect_1, rect_0):
+            #             print(rect_1, rect_0)
+            remove_indexes.append(index_pair[0])
+
+    keep_indexes = [ind for ind in range(len(cnts)) if ind not in remove_indexes]
+
+    cnts_ = [cnts[ind] for ind in keep_indexes]
+
+    return cnts_
+
+def get_contours(mask):
+    masks = []
+    mask = mask * 255
+    mask = mask.astype('uint8')
+    # cv2.imwrite('current_mask.jpg', mask)
+    
+    """
+    draw a black line on all 4 corners of the image,
+    this is done to avoid open contours later on
+    """
+    height, width = mask.shape
+    cv2.line(mask, (0, 0), (0, height), (0, 0, 0), thickness=5)
+    cv2.line(mask, (0, 0), (width, 0), (0, 0, 0), thickness=5)
+    cv2.line(mask, (0, height), (width, height), (0, 0, 0), thickness=5)
+    cv2.line(mask, (width, 0), (width, height), (0, 0, 0), thickness=5)
+    
+    edged = cv2.Canny(mask, 10, 30)
+    kernel = np.ones((5, 5), np.uint8)
+    edged = cv2.dilate(edged, kernel, iterations=1)
+
+    cnts = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    
+    cnts = remove_contained_cnts(cnts)
+    
+    for cnt in cnts:
+        img_cnt = np.zeros((mask.shape[0], mask.shape[1]))  # create a single channel 200x200 pixel black image plt.imshow(img_cnt)
+        img_cnt = cv2.fillPoly(img_cnt, pts=[cnt], color=(255, 255, 255))
+        # masks.append((np.logical_and(img_cnt, mask)*255).astype('uint8'))
+        masks.append(np.logical_and(img_cnt, mask))
+        # cv2.imwrite('contour.jpg', np.logical_and(img_cnt, mask)*255)
+
+    return masks
+
 def process_im_path(path, saliency_path):
     saliency_mask = cv2.imread(saliency_path, 0)
     img = read_image(path, format="BGR")
@@ -141,30 +217,33 @@ def process_im_path(path, saliency_path):
     
     
     for mask in masks:
-        lookup = np.where(mask==True, saliency_mask/255.0 , 0)
-        total_pixels = np.count_nonzero(mask)
-         
-        # lookup = np.where(lookup > 0.05, lookup, 0)
+        #function to split mask into separate contours
+        cnts = get_contours(mask)
         
-        if not np.all(lookup == 0.) and total_pixels > 0:
-            sad_diff, mse_diff = sad_calculation(mask, lookup)
-            cv2.imwrite('lookup.jpg', lookup*255)
-            cv2.imwrite('mask.jpg', mask*255)
-            # lookup = saliency_mask[mask]
-            # lookup_pixels = np.count_nonzero(lookup)
-            thresh = sad_diff/total_pixels
-            confidence = 1. - thresh
-            encountered_pixels.append(mask)
+        for cnt in cnts:
+            lookup = np.where(cnt==True, saliency_mask/255.0 , 0)
+            total_pixels = np.count_nonzero(cnt)
             
-            if confidence > 0.85:
-            # if 0.7 < confidence < 0.85:
-                # result[mask] = mask
-                result.append(mask)
+            # lookup = np.where(lookup > 0.05, lookup, 0)
+            
+            if not np.all(lookup == 0.) and total_pixels > 0:
+                sad_diff, mse_diff = sad_calculation(cnt, lookup)
+                # cv2.imwrite('lookup.jpg', lookup*255)
+                # cv2.imwrite('mask.jpg', cnt*255)
+                # lookup = saliency_mask[mask]
+                # lookup_pixels = np.count_nonzero(lookup)
+                thresh = sad_diff/total_pixels
+                confidence = 1. - thresh
+                encountered_pixels.append(cnt)
                 
-                append_count += 1
-        temp = 0
+                if confidence > 0.85:
+                # if 0.7 < confidence < 0.85:
+                    # result[mask] = mask
+                    result.append(cnt)     
+                    append_count += 1
+            # temp = 0
     
-    #to do : combine encountered pixels and saliency mask
+    #combine encountered pixels and saliency mask
     if len(result) > 0:
         result = np.array(result)
         result = np.sum(result, axis=0)
@@ -210,8 +289,8 @@ def process_im_path(path, saliency_path):
         # cv2.imwrite(out_filename, mask)
         cv2.imwrite(out_filename, corrected_result)
         # cv2.imwrite(saliency_path, saliency_mask)
-        cv2.imwrite('pixels_encountered.jpg', encountered_pixels)
-        cv2.imwrite('result.jpg', result)
+        # cv2.imwrite('pixels_encountered.jpg', encountered_pixels)
+        # cv2.imwrite('result.jpg', result)
         
     else:
         cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
